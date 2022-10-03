5baf993675d14d25c63be52ecaf08c1da968aae5
ssonhub <58754723+ssonhub@users.noreply.github.com>
2022-10-03 03:53:57 -0700

    added floating button
    spread operator

--- budget_tracker/lib/pages/home_page.dart
+++ budget_tracker/lib/pages/home_page.dart
@@ -2,55 +2,101 @@ import 'package:budget_tracker/model/transaction_item.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
-class HomePage extends StatelessWidget {
+/* For us to be able to display the cards when we click a button,
+ we need some sort of state management that holds the List of cards 
+ being displayed on the screen */
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  /* Let's create a list of transaction items to be displayed now.
+  we'll add and remove from that List to be able to populate our transactions.
+  The List will be of TransactionItem and will be empty to start */
+  List<TransactionItem> items = [];
 
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: SizedBox(
-          width: screenSize.width,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Align(
-                alignment: Alignment.topCenter,
-                child: CircularPercentIndicator(
-                  radius: screenSize.width / 2,
-                  lineWidth: 10.0,
-                  percent: .5,
-                  backgroundColor: Colors.white,
-                  center: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const [
-                      Text(
-                        "\$0",
-                        style: TextStyle(
-                            fontSize: 48, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "Balance",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ],
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(
+            () {
+              items.add(
+                TransactionItem(amount: 5.99, itemTitle: "Food"),
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: SizedBox(
+            width: screenSize.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: CircularPercentIndicator(
+                    radius: screenSize.width / 2,
+                    lineWidth: 10.0,
+                    percent: .5,
+                    backgroundColor: Colors.white,
+                    center: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Text(
+                          "\$0",
+                          style: TextStyle(
+                              fontSize: 48, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Balance",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
+                    progressColor: Theme.of(context).colorScheme.primary,
                   ),
-                  progressColor: Theme.of(context).colorScheme.primary,
                 ),
-              ),
-              const SizedBox(
-                height: 35,
-              ),
-              const Text(
-                "Items",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-            ],
+                const SizedBox(
+                  height: 35,
+                ),
+                const Text(
+                  "Items",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+
+                /* you want the entire screen to be scrollable, not just the list
+      (whwere you could achieve it with a ListView.builder()) 
+      
+      we'll call a generate method that is part of the List. This method takes
+      in the size of the list as a parameter and provides a generator, which is
+      just a callback with an int parameter representing the index that
+      returns an index for each item */
+
+                /* The only problem with this code is that it returns a List<Widget> and
+      our column won't accept that. Thankfully we can use the spread operator (...) */
+
+                ...List.generate(
+                  items.length,
+                  (index) => TransactionCard(
+                    item: items[index],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
