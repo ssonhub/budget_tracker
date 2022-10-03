8ee496be64e21d799c216552871e385e488a8287
ssonhub <58754723+ssonhub@users.noreply.github.com>
2022-10-03 00:21:43 -0700

    added current page index

--- budget_tracker/lib/screens/home.dart
+++ budget_tracker/lib/screens/home.dart
@@ -15,7 +15,25 @@ class _HomeState extends State<Home> {
   List<BottomNavigationBarItem> bottomNavItems = const [
     BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
     BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+
+    /* If you try clicking on the bottom navigation bar, nothing happens
+    because we haven't given any onTap: functionality. */
   ];
+
+  List<Widget> pages = const [
+    HomePage(),
+    ProfilePage(),
+  ];
+
+  /* if we wanted to access the HomePage, we could do pages[0] and pages[1]
+    would give us Profile. Because onTap: gives us the index when we press,
+    we can use that to change the body: of our widget to render each page
+    
+    To do that, we first need to declare a _currentPageIndex variable that
+    will hold the current selected page index and will be the one we'll use to
+    set it */
+
+  int _currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -23,8 +41,16 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: const Text("Budget Tracker"),
       ),
-      body: Container(),
-      bottomNavigationBar: BottomNavigationBar(items: bottomNavItems),
+      // parameter we can call pages[_crrentPageIndex]
+      // This will render the page selection accordingly
+      body: pages[_currentPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: bottomNavItems,
+        /* BottomNavigationBar widget has an onTap(int) 
+        when we click the Home icon, it would print out 0 and for the Profile
+        icon, it would print out 1*/
+        onTap: (index) => print(index),
+      ),
     );
   }
 }
